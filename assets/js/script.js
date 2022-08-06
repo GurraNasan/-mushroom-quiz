5c5353b753a51bb0b228b869c322e69cb5ac47fb
GurraNasan <gurra.naslund@gmail.com>
2022-08-06 14:47:08 +0000

    add variabls and eventlistner for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-startGame(); {
+/* Variabel for buttons */
+const askQuestion = document.getElementById("next-button");
+const answerOne = document.getElementById("answer_one"); 
+const answerTwo = document.getElementById("answer_two"); 
+const answerThree = document.getElementById("answer_three"); 
+
+/* Eventlisteners */
+
+askQuestion.addEventListener('click', startGame);
+
+
+function startGame() {
+    alert("Start game")
+    console.log("Start game")
+}
+
+function question() {
+
+};
+
+function shuffle() {
 
 }
 
-question(); {
+function checkAnswer() {
+
+}
+function correct() {
 
 }
 
-shuffle(); {
-
-}
-
-checkAnswer(); {
-
-}
-correct (); {
-
-}
-
-incorrect (); {
+function incorrect() {
 
 }
